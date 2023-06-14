a8b809f627a0371e8693721380dee284323ce9d2
MustSave <ysh99@korea.ac.kr>
2023-06-14 22:01:56 +0900

    init project

---
+++ src/test/java/spring/mongo/board/BoardApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.mongo.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
